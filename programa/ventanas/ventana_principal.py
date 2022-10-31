51d070f34808da1a710d1ab5c4074440bc2f1e20
Hansol A. Rostrán <89949216+hros19@users.noreply.github.com>
2022-10-30 21:41:45 -0600

    Se quitó la música y se agregó funcionalidad
    Ya está lista la navegabilidad de la ventana y es funcional.

--- programa/ventanas/ventana_principal.py
+++ programa/ventanas/ventana_principal.py
@@ -1,79 +1,104 @@
 # Módulos de terceros
 import tkinter as tk
+import os
 import time
 from PIL import Image, ImageTk, ImageSequence
+from ventanas.ventana_estadisticas import VentanaEstadisticas
 
 # Módulos propios
-import modelos.reproductor as AudioManager
+from ventanas.ventana_config import VentanaConfig
 
 class VentanaPrincipal:
-    PATH_FONDO_ANIMADO = "./programa/multimedia/fondo_animado.gif"
-    PATH_IMG_AUDIO_ON = "./programa/multimedia/audio_encendido.png"
-    PATH_IMG_AUDIO_OFF = "./programa/multimedia/audio_apagado.png"
+  """Ventana de inicio de la aplicación con el menú principal
+  """
+  
+  PATH_FONDO_ANIMADO = (os.getcwd() + "./programa/multimedia/fondo_animado.gif")
+  ventana_activa = True
     
-    def __init__(self):
-        # Iniciar la musica
-        AudioManager.reproducir_musica()
-        
-        # Propiedades de la ventana
-        self.ventana = tk.Tk()
-        self.ventana.title('Laberinto')
-        self.ventana.geometry('900x900')
-        self.ventana.resizable(False, False)
-        
-        # Background animado - gif
-        self.gif_label = tk.Label(self.ventana, bg = 'black')
-        self.gif_label.place(x = 0, y = 0)
-        
-        # Imagen de audio
-        self.IMG_AUDIO_ON = tk.PhotoImage(file = self.PATH_IMG_AUDIO_ON).subsample(7, 6)
-        self.IMG_AUDIO_OFF = tk.PhotoImage(file = self.PATH_IMG_AUDIO_OFF).subsample(7, 6)
-        
-        # Botón de pausa
-        self.btn_musica = tk.Button(
-            self.ventana, 
-            text = 'Pausar',
-            width = 120,
-            height = 100,
-            image = self.IMG_AUDIO_ON,
-            command = lambda: self.alternar_musica(),
-            bg = 'light blue',
-            highlightcolor = 'white',
-            relief = tk.GROOVE,
-            compound = tk.TOP
-        )
-        self.btn_musica.place(x = 25, y = 25)
-        
-        self.animar_background()
+  def __init__(self):
+    # Propiedades de la ventana
+    self.ventana = tk.Tk()
+    self.ventana.title('Laberinto')
+    self.ventana.resizable(False, False)
+    self.ventana.eval('tk::PlaceWindow . center')
+    self.ventana.geometry('{}x{}+{}+{}'.format(
+      900, 900,
+      self.ventana.winfo_screenwidth() // 2 - 900 // 2,
+      self.ventana.winfo_screenheight() // 2 - 900 // 2 - 50
+    ))
     
-    def animar_background(self):
-        gif_media = Image.open(self.PATH_FONDO_ANIMADO)
-        
-        for frame in ImageSequence.Iterator(gif_media):
-            frame = frame.resize((900, 900))
-            frame_nuevo = ImageTk.PhotoImage(frame)
-            self.gif_label.configure(image = frame_nuevo)
-            self.ventana.update()
-            time.sleep(0.05)
-        self.ventana.after(0, self.animar_background())
-        
-    def alternar_musica(self):
-        # Se alterna la música y se cambia el texto e imagen del botón
-        if (AudioManager.musica_encendida):
-            AudioManager.pausar_musica()
-            self.btn_musica.configure(
-                text = 'Reproducir',
-                image = self.IMG_AUDIO_OFF,
-                command = lambda: self.alternar_musica(),
-                compound = tk.TOP
-            )
-            AudioManager.musica_encendida = False
-            
-        else:
-            AudioManager.continuar_musica()
-            self.btn_musica.configure(
-                text = 'Pausar',
-                image = self.IMG_AUDIO_ON,
-                compound = tk.TOP
-            )
-            AudioManager.musica_encendida = True
+    # Background animado - gif
+    self.gif_label = tk.Label(self.ventana, bg = 'black')
+    self.gif_label.place(x = 0, y = 0)
+    
+    # Botón para iniciar el juego
+    self.btn_iniciar = tk.Button(
+      self.ventana,
+      bg='black',
+      fg='white',
+      width=18,
+      font=('Arial', 15),
+      text='Jugar',
+      command=lambda: self.abrir_ventana_config()
+    )
+    self.btn_iniciar.place(x = 645, y = 450)
+    
+    self.btn_estadisticas = tk.Button(
+      self.ventana,
+      bg='black',
+      fg='white',
+      width=18,
+      font=('Arial', 15),
+      text='Partidas',
+      command=lambda: self.abrir_ventana_estadisticas()
+    )
+    self.btn_estadisticas.place(x = 645, y = 500)
+    
+    # Botón para salir de la aplicación
+    self.btn_salir = tk.Button(
+      self.ventana,
+      bg='red',
+      fg='white',
+      width=18,
+      font=('Arial', 15),
+      text='Salir',
+      command=lambda: self.cerrar_ventana()
+    )
+    self.btn_salir.place(x = 645, y = 550)
+    
+    self.animar_background()
+    self.ventana.mainloop()
+  
+  def abrir_ventana_estadisticas(self):
+    self.ventana_activa = False
+    self.ventana.quit()
+    self.ventana.destroy()
+    vent_est = VentanaEstadisticas()
+  
+  def abrir_ventana_config(self):
+    self.ventana_activa = False
+    self.ventana.quit()
+    self.ventana.destroy()
+    vent_config = VentanaConfig()
+    
+  def cerrar_ventana(self):
+    self.ventana_activa = False
+    self.ventana.quit()
+    self.ventana.destroy()
+
+  def animar_background(self):
+    gif_media = Image.open(self.PATH_FONDO_ANIMADO)
+    
+    for frame in ImageSequence.Iterator(gif_media):
+      if (self.ventana_activa):    
+        frame = frame.resize((900, 900))
+        frame_nuevo = ImageTk.PhotoImage(frame)
+        self.gif_label.configure(image = frame_nuevo)
+        self.ventana.update()
+        time.sleep(0.05)
+      else:
+        self.ventana.after_cancel(self.animar_background)
+        return
+    self.ventana.after(1, self.animar_background())
+    
+    
